8029980ed05e7d82902fa4ffa28768bab4ffd311
Serhii Buria <sergeiburya@gmail.com>
2023-01-19 22:59:09 +0200

    added UserController

--- src/main/java/com/example/springbootdemousermongodb/controller/UserController.java
+++ src/main/java/com/example/springbootdemousermongodb/controller/UserController.java
@@ -3,11 +3,16 @@ package com.example.springbootdemousermongodb.controller;
 import com.example.springbootdemousermongodb.dto.UserMapper;
 import com.example.springbootdemousermongodb.dto.UserRequestDto;
 import com.example.springbootdemousermongodb.dto.UserResponseDto;
+import com.example.springbootdemousermongodb.model.User;
+import com.example.springbootdemousermongodb.repository.UserRepository;
 import com.example.springbootdemousermongodb.service.UserService;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     private final UserMapper mapper;
+    private final UserRepository userRepository;
 
-    public UserController(UserService userService, UserMapper mapper) {
+    public UserController(UserService userService, UserMapper mapper,
+                          UserRepository userRepository) {
         this.userService = userService;
         this.mapper = mapper;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/add-user")
     public UserResponseDto create(@RequestBody UserRequestDto requestDto) {
         return mapper.toDto(userService.create(mapper.toModel(requestDto)));
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDto update(@PathVariable String id,
+                                  @RequestBody UserRequestDto requestDto) {
+        User user = mapper.toModel(requestDto);
+        user.setId(id);
+        return mapper.toDto(userService.create(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        userRepository.deleteAllById(id);
     }
 
     @GetMapping
--- src/main/java/com/example/springbootdemousermongodb/repository/UserRepository.java
+++ src/main/java/com/example/springbootdemousermongodb/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
     List<User> findAll();
+
+    void deleteAllById(String id);
 }
