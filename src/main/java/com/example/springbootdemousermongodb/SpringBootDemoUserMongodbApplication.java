a94766b9f38b090f212cd20c5e1b7087ee14f8ce
Serhii Buria <sergeiburya@gmail.com>
2023-01-19 20:13:54 +0200

    initial

---
+++ src/main/java/com/example/springbootdemousermongodb/SpringBootDemoUserMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemousermongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoUserMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoUserMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdemousermongodb/SpringBootDemoUserMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemousermongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoUserMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
